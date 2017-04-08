e04faa34b13090059c7e7c6929812c27d01cb64c
StartUPIX <StartUPIX>
2017-04-08 13:05:00 +0400

    test

--- src/test/java/aca/first/test/MyFirstWebDriverTest.java
+++ src/test/java/aca/first/test/MyFirstWebDriverTest.java
@@ -12,19 +12,19 @@ public class MyFirstWebDriverTest {
 	/* 1) bacel kayq@ 2) gnal "News and Career"  */
 	@Test()
 	public void myFirstTest(){
-		//System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Java\\chromedriver_win32\\chromedriver.exe");
-		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Java\\chromedriver_win32\\chromedriver.exe");
 		
-		System.setProperty("webdriver.gecko.driver", "C:\\Program Files\\Java\\Geckodriver\\geckodriver.exe");
-		WebDriver driver = new FirefoxDriver();
-		//WebDriver driver=new ChromeDriver();
+		//System.setProperty("webdriver.gecko.driver", "C:\\Program Files\\Java\\Geckodriver\\geckodriver.exe");
+		//WebDriver driver = new FirefoxDriver();
+		WebDriver driver=new ChromeDriver();
 		 //driver.get("http://aca.am");
 		 driver.get("https://fairygodboss.com/");
 		 String URL = driver.getCurrentUrl();
+		 System.out.println("URL is "+ URL);
 		 Assert.assertEquals(URL, "https://fairygodboss.com/");
 		 driver.findElement(By.xpath("//a[@href='/salary-database']")).click();
+		
 		 driver.close();
-		 driver.quit();
 	}
 
 }
