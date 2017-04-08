fb1753b6889a8293c34ef1af3dff020befbe8e6e
Vika <Vika>
2017-04-08 14:37:42 +0400

    LinkedIn

--- src/test/java/aca/first/test/MyFirstWebDriverTest.java
+++ src/test/java/aca/first/test/MyFirstWebDriverTest.java
@@ -1,16 +1,18 @@
 package aca.first.test;
 
+
+
 /*/*
 Test Case 01
 Description: Create new user on www.linkedIn.com
 Steps:
 1. Navigate to www.linkedIn.com;
 2. Registration new user;
-3. Enter First Name and Last Name Fields;
-4. Enter correct E-mail address and password (6 or more characters);
-5. Click "Join Now" button 
+3. Enter E-mail address;
+4. Enter password;
+5. Click "Sign In" button 
 
-Expected Result: Authorization should be able to succesfull, displaying the linkedIn user`s home page.
+Expected Result: Page should be able to open succesfull, displaying the linkedIn user`s home page.
 */
 
 
@@ -26,8 +28,7 @@ public class MyFirstWebDriverTest {
 	public void myFirstTest(){
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Vika\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		 WebDriver driver = new ChromeDriver();
-		 driver.get("http://aca.am");
-		 
+		 	 
 		 
 			  driver.get("https://www.linkedin.com/");
 			  driver.manage().window().maximize();
@@ -42,7 +43,10 @@ public class MyFirstWebDriverTest {
 			  driver.findElement(By.id("login-password")).sendKeys("525252");
 			  driver.findElement(By.id("login-submit")).click();
 			 
-			  
+			    String my_linkedinpage=driver.getTitle();
+				System.out.println("Title is" +my_linkedinpage);
+				assert true;
+				System.out.println("Test complited");
 		 
 		 
 		 
